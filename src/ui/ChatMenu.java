9668e632d5fb884aea37eb6ca3e3be4d477dd91e
christopherhui <christophergkhui@gmail.com>
2018-12-01 17:19:24 -0800

    Added gui visuals, need backend

--- src/ui/ChatMenu.java
+++ src/ui/ChatMenu.java
@@ -15,13 +15,19 @@ public class ChatMenu extends JFrame {
 
     public void main() {
         getContentPane().setBackground(Color.WHITE);
-        setSize(500, 500);
+        setSize(500, 700);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setResizable(true);
 
-        JPanel cards = new JPanel(new GridLayout(3,0));
+        String[] genderTypes = {"Straight", "Gay", "Bisexual", "Other"};
+        String[] ethnicityTypes = {"White", "Black", "Asian", "Latino", "Other"};
+        String[] ageRange = {"12-16","17-24","25-35","36-50","51+"};
+        JPanel cards = new JPanel(new GridLayout(6,1));
         JPanel characterProfile = new JPanel();
         JPanel characterOptions = new JPanel();
+
+        JPanel characterName = new JPanel();
+        JPanel contactInfo = new JPanel();
 
         try {
             BufferedImage image = ImageIO.read(this.getClass().getResource("logo.jpg"));
@@ -31,16 +37,30 @@ public class ChatMenu extends JFrame {
             System.out.println("not found!");
         }
 
-        characterProfile.add(new JButton("test"));
-        characterProfile.add(new JButton("test2"));
+        characterProfile.add(new JLabel("Username:"));
+        characterProfile.add(new JTextArea(1,10));
 
-        characterOptions.add(new JButton("test3"));
+        characterName.add(new JLabel("Name:"));
+        characterName.add(new JTextArea(1,10));
+
+        contactInfo.add(new JLabel("Contact Information:"));
+        contactInfo.add(new JTextArea(2, 10));
+
+        characterOptions.add(new JLabel("Set your preferences:"));
+        characterOptions.add(new JComboBox<>(genderTypes));
+        characterOptions.add(new JComboBox<>(ethnicityTypes));
+        characterOptions.add(new JComboBox<>(ageRange));
 
         cards.add(characterProfile);
         cards.add(characterOptions);
+        cards.add(characterName);
+        cards.add(contactInfo);
+        JButton signUp = new JButton("Sign up!");
+        cards.add(signUp);
         add(cards, BorderLayout.CENTER);
         cards.setBorder(BorderFactory.createEmptyBorder(30,10,10,10));
 
+        pack();
         setLocationRelativeTo(null);
         setVisible(true);
     }
