75165839689c61daa28aacb02571fde53b938b2c
christopherhui <christophergkhui@gmail.com>
2018-12-01 17:36:12 -0800

    Added image

---
+++ src/ui/openChatBox.java
@@ -0,0 +1,11 @@
+package ui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class openChatBox implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
